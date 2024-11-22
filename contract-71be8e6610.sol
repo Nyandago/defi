afc1c0e25339eda1664d29745b3a8989f100824c
Nyandago <paulmutator@gmail.com>
2024-11-22 14:21:13 +0300

    complete DeFi project

---
+++ contract-71be8e6610.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC20} from "@openzeppelin/contracts@5.1.0/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts@5.1.0/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts@5.1.0/access/Ownable.sol";
+
+/// @custom:security-contact info@canny.tz
+contract CannyToken is ERC20, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("CannyToken", "CTK")
+        Ownable(initialOwner)
+        ERC20Permit("CannyToken")
+    {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
